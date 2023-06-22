6cde3aa81d76b9e72a9422fa6d9f613f1400d28b
Sterling <sterlingduncan9@gmail.com>
2023-06-22 11:25:04 -0400

    added todo , strikethrough todo, and remove todo functionality working

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 //bring in elements from todo list
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const todoUL = document.getElementById("todos");
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todoUL = document.getElementById('todos');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    // console.log(e)
 
-
-    
+    addTodo();
 })
+
+function addTodo(todo) {
+    //saving the input value (text) to a variable
+    let todoText = input.value
+
+    //checking if a todo exists
+    if(todo) {
+        //set the value of inpu to the value of todo.text
+        todoText = todo.text 
+    }
+    
+    // if the text exists 
+    if(todoText) {
+        // create a new list item
+        const todoEL = document.createElement('li');
+
+        // if there is a todo AND its completed    
+        if(todo && todo.completed) {
+            // add strike through class
+            todo.classList.add('completed')
+        }
+        //make the text of li same as input value
+            todoEL.innerText = todoText;
+        // append the todo li item to the todo unordered list
+            todoUL.appendChild(todoEL);
+            //clear out after enter
+            input.value = " "
+
+            todoEL.addEventListener('click', () => {
+                todoEL.classList.toggle('completed');
+            })
+             todoEL.addEventListener('contextmenu', (e)=> {
+                e.preventDefault();
+                
+                //remove list item
+                 todoEL.remove()
+             })
+    }
+
+}
+
